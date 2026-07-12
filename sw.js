31a4ff702bed36cb7cea42d81aabad6c7349f174
jonatascjacob-ops <jonatascjacob-ops@users.noreply.github.com>
2026-07-12 08:37:11 -0300

    Adiciona suporte offline completo, Service Worker e log invisivel

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'devocional-cld-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './assets/tailwind.js',
+  './assets/chart.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
